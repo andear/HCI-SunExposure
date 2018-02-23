38d614df7c01265ab3af321e13c328312893315a
andear <andear@andeardeMacBook-Pro.local>
2018-02-23 12:15:56 -0600

    modify profile

---
+++ SunExposure.js
@@ -0,0 +1,9 @@
+function modifyButton(){
+    document.getElementById('modify').style.display='block';
+    document.getElementById("present").style.display='none';
+}
+
+function saveProfile(){
+    document.getElementById('present').style.display='block';
+    document.getElementById("modify").style.display='none';
+}
